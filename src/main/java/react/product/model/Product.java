b671dcbed7967fea2c2937db4cda663fa5814241
gaurav-xfactrs <gaurav.kumar@synthesis-systems.com>
2024-05-20 21:12:56 +0530

    Entity for product created

---
+++ src/main/java/react/product/model/Product.java
@@ -0,0 +1,20 @@
+package react.product.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Double price;
+    private Integer quantity;
+}
